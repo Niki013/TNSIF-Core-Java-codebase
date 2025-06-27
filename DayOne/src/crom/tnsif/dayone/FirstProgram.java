01e08949c1828198183a908286d3edd8243e1299
YTECH <YTECH@DESKTOP-IIR05J0>
2025-06-27 15:25:18 +0530

    Day1 TNSIF

---
+++ DayOne/src/crom/tnsif/dayone/FirstProgram.java
@@ -0,0 +1,10 @@
+package crom.tnsif.dayone;
+
+public class FirstProgram {
+	//requried to run class file
+	public static void main(String[] args) {
+		System.out.println(" Hello World!  ");
+		
+	}
+	
+}
