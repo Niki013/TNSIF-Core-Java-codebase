3746948e0d180d7e1417b52b23452e2bffdb7630
YTECH <YTECH@DESKTOP-IIR05J0>
2025-07-07 19:53:56 +0530

    dayseven

---
+++ DaySeven/src/com/tnsif/datseven/overriding/RBI.java
@@ -0,0 +1,10 @@
+package com.tnsif.datseven.overriding;
+
+public class RBI {
+
+	public float getRateofInterest()
+	{
+		System.out.println("Base rate of interest of home loan");
+		return 6.7f;
+	}
+}
