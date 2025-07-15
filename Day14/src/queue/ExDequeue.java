759c7622938d609e53bba295115c791fa47fa6fa
YTECH <YTECH@DESKTOP-IIR05J0>
2025-07-15 10:20:22 +0530

    day14

---
+++ Day14/src/queue/ExDequeue.java
@@ -0,0 +1,24 @@
+package queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ExDequeue {
+
+	public static void main(String[] args) {
+	Deque<String > deque1=new ArrayDeque<String>();
+	deque1.add("Gautami");
+	deque1.add("Kiran");
+	
+	System.out.println(deque1);
+	
+	deque1.pollLast();
+	System.out.println(deque1);
+	
+	deque1.pollFirst();
+	System.out.println(deque1);
+	
+	
+	}
+
+}
